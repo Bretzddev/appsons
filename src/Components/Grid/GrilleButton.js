829295c29a6f9d78b18f59cda49c879093183462
Breztdev <129894065+Bretzddev@users.noreply.github.com>
2023-08-09 17:56:53 +0200

    Styled Button

--- src/Components/Grid/GrilleButton.js
+++ src/Components/Grid/GrilleButton.js
@@ -1,9 +1,39 @@
 import { styled } from "styled-components";
 
-export default function GrilleButton(){
-    return <Wrapper></Wrapper>;
+export default function GrilleButton({isPlayed=false}){
+    return <Wrapper isPlayed={isPlayed}></Wrapper>;
     }
 
     const Wrapper = styled.div`
-    border: solid 1px;
+    border-radius: 4px;
+    background: rgba(213, 236, 194, 1);
+    background: radial-gradient(circle, rgba(213, 236, 194, 1) 0%, rgba(152, 221, 202, 1)100%);
+    position: relative;
+    &::before {
+        position: absolute;
+        content: "";
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 0;
+        background: radial-gradient(
+            circle,
+            rgba(255, 170, 167, 1) ${(props) => (props.isPlayed ? "25%" : "0%")},
+            rgba(255, 230, 213, 1) 100%
+        );
+        opacity: ${(props) => (props.isPlayed ? "1" : "0")};
+        transition: linear 0.2s;
+    }
+    &:hover::before{
+        opacity: 1;
+    }
+    &:active::before{
+        opacity: 1;
+        background: radial-gradient(
+            circle,
+            rgba(255, 170, 167, 1) 25%,
+            rgba(255, 230, 213, 1) 100%
+        );
+    }
     `;
