f65e27c0f5d0c80a51c5b6253d79d0b8114848f4
Breztdev <129894065+Bretzddev@users.noreply.github.com>
2023-08-10 09:03:35 +0200

    Keyboard Effect

--- src/hooks/useSounds.js
+++ src/hooks/useSounds.js
@@ -1,19 +1,20 @@
 import * as Tone from "tone";
 import { useEffect, useRef } from "react";
+import Kick from "assets/sounds/Kick.wav"
+import Loop from "assets/sounds/Loop14.wav"
+import Perc from "assets/sounds/Perc1.wav"
+import Snare from "assets/sounds/Snare.wav"
+
 
 export default function useSounds(){
 const mySampler = useRef(null);
 
 useEffect(()=> {
     const sampler = new Tone.Sampler({
-        urls: {
-            C4: "C4.mp3",
-            "D#4": "Ds4.mp3",
-            "F#4": "Fs4.mp3",
-            A4: "A4.mp3",
-        },
-        release: 1,
-        baseUrl: "https://tonejs.github.io/audio/salamander/",
+        C4: Kick,
+        "D#4": Loop,
+        "F#4": Perc,
+        A4: Snare,
     }).toDestination();
 
     Tone.loaded().then(() => {
@@ -21,11 +22,31 @@ useEffect(()=> {
     });
 }, []);
 
+function soundPlay(note) {
+    mySampler.current.triggerAttackRelease([note], 4);
+}
+
+function handleKeyDown({key}){
+switch (key){
+    case "a": soundPlay("C4");break;
+    case "z": soundPlay("D#4");break;
+    case "e": soundPlay("F#4");break;
+    case "r": soundPlay("A4");break;
+default: break;
+}}
+
+useEffect(()=>{
+    window.addEventListener("keydown", handleKeyDown);
+return ()=> {
+    window.removeEventListener("keydown", handleKeyDown);
+};
+}, []);
+
 const buttonsList = [
-    {soundPlay: () => mySampler.current.triggerAttackRelease(["C4"], 4)},
-    {soundPlay: () => mySampler.current.triggerAttackRelease(["D#4"], 4)},
-    {soundPlay: () => mySampler.current.triggerAttackRelease(["F#4"], 4)},
-    {soundPlay: () => mySampler.current.triggerAttackRelease(["A4"], 4)},
+    {soundPlay: () => soundPlay("C4"),},
+    {soundPlay: () => soundPlay("D#4"),},
+    {soundPlay: () => soundPlay("F#4"),},
+    {soundPlay: () => soundPlay("A4"),},
  ];
     return {buttonsList};
 }
